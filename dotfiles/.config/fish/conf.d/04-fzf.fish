5aa422d6204a3dc8433385fab3b6aef860029cfe
syaofox <syaofox@gmail.com>
2026-05-03 21:36:15 +0800

    123132

--- dotfiles/.config/fish/conf.d/04-fzf.fish
+++ dotfiles/.config/fish/conf.d/04-fzf.fish
@@ -2,15 +2,9 @@
 
 if status is-interactive
     stty -ixon
-    
+
     set fzf_preview_dir_cmd exa --all --color=always  # 使用 exa 代替 ls 预览目录
     set fzf_fd_opts --hidden --exclude=.git          # 让 fd 搜索隐藏文件但排除 .git
 
-    # 先声明你想用的快捷键
-    fzf_configure_bindings \
-        --history=\cr \
-        --directory=\co \
-        --processes=\cp \
-        --git_log=\cg \
-        --git_status=\cs 
+  
 end
---
+++ dotfiles/.config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,8 @@
+function fish_user_key_bindings
+    fzf_configure_bindings \
+        --history=\cr \
+        --directory=\co \
+        --processes=\cp \
+        --git_log=\cg \
+        --git_status=\cs
+end
