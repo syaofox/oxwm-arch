d1e5622fba826f8e55d6aab89fc17b7c91dbc0c1
syaofox <syaofox@gmail.com>
2026-05-03 21:22:14 +0800

    123123

--- dotfiles/.config/fish/conf.d/04-fzf.fish
+++ dotfiles/.config/fish/conf.d/04-fzf.fish
@@ -1,6 +1,8 @@
 # 查看所有绑定 fish -i -c "bind"
 
 if status is-interactive
+    stty -ixon
+    
     set fzf_preview_dir_cmd exa --all --color=always  # 使用 exa 代替 ls 预览目录
     set fzf_fd_opts --hidden --exclude=.git          # 让 fd 搜索隐藏文件但排除 .git
 
