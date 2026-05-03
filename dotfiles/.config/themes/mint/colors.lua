4556659b042ffedfaf539aaabd45e465f51c94da
syaofox <syaofox@gmail.com>
2026-05-03 10:41:44 +0800

    新增mint配色

---
+++ dotfiles/.config/themes/mint/colors.lua
@@ -0,0 +1,19 @@
+return {
+    fg = "#ebf7f7",
+    red = "#f7768e",
+    bg = "#0c1414",
+    sep = "#3a5858",
+    cyan = "#00c4b0",
+    green = "#80c8a0",
+    lavender = "#6dc0ba",
+    light_blue = "#4ab8c5",
+    grey = "#a8baba",
+    blue = "#3da8b8",
+    purple = "#1A8693",
+
+    text = "#ddf8f8",
+    surface = "#1c3232",
+    yellow = "#e0af68",
+    accent = "#28b8a5",
+    muted = "#586b6b",
+}
