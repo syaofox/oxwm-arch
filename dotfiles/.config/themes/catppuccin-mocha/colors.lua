16afbfbbb8e51323ce36271df0bb69b8fa6ae7d9
syaofox <syaofox@gmail.com>
2026-05-03 10:46:45 +0800

    添加 catppuccin-mocha 配色

---
+++ dotfiles/.config/themes/catppuccin-mocha/colors.lua
@@ -0,0 +1,19 @@
+return {
+    fg = "#cdd6f4",
+    red = "#f38ba8",
+    bg = "#1e1e2e",
+    sep = "#585b70",
+    cyan = "#94e2d5",
+    green = "#a6e3a1",
+    lavender = "#b4befe",
+    light_blue = "#89b4fa",
+    grey = "#a6adc8",
+    blue = "#74c7ec",
+    purple = "#cba6f7",
+
+    text = "#cdd6f4",
+    surface = "#313244",
+    yellow = "#f9e2af",
+    accent = "#cba6f7",
+    muted = "#6c7086",
+}
